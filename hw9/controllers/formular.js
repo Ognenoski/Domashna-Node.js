0e8405ad1932999a668b98ff249ae0451525a390
Dani <ognenoski777dani@gmail.com>
2026-07-20 17:07:47 +0200

    hw9

---
+++ hw9/controllers/formular.js
@@ -0,0 +1,60 @@
+const { list, add, remove, update,getOne } = require("../models/student");
+
+const getForm = (req, res) => {
+    res.render("formular");
+};
+
+const postForm = async (req, res) => {
+    let studentData = {
+        ime: req.body.ime,
+        prezime: req.body.prezime,
+        prosek: req.body.prosek
+    };
+    await add(studentData);
+    res.redirect("/students"); // will navigate the client(user) to /students
+    // by default the user will be sent to the GET method page of /students
+};
+
+const getStudents = async(req, res) => {
+    let data = await list();
+    res.render("students", { data });
+};
+
+const getDelete = async (req, res) => {
+    console.log("req query", req.query);
+    await remove(req.query.studentIndex); //how will we name the variable in the ejs file
+    res.redirect("/students");
+};
+const getEdit = async (req, res) => {
+    const index = req.query.studentIndex;
+
+    const student = await getOne(index);
+
+    res.render("edit", {
+        student,
+        index
+    });
+};
+const postEdit = async (req, res) => {
+
+    const index = req.body.index;
+
+    const studentData = {
+        ime: req.body.ime,
+        prezime: req.body.prezime,
+        prosek: req.body.prosek
+    };
+
+    await update(index, studentData);
+
+    res.redirect("/students");
+};
+
+module.exports = {
+    getForm,
+    postForm,
+    getStudents,
+    getDelete,
+    getEdit,
+    postEdit
+}
---
+++ hw9/index.js
@@ -0,0 +1,48 @@
+//MVC pattern
+//Model 
+//View
+//Controller
+
+// Na ovoj cas ke napravime aplikacija za dodavanje i brisenje studenti so ime, prezime, prosek
+// Arhitektura na aplikacijata:
+// -> Frontend-UI (Kaj nas toa ke bide views folderot views(html ili ejs)) ->
+// -> Router (Kaj nas na ovoj cas nema da ima potreba za toa bidejki ke se malku ruti, nema da ima poseben router folder, site ruti ke gi stavime vo index.js) ->
+// -> Controller (tuka ni e programerskata logika, odnosno handler) ->
+// -> Model (Kako ke ni izgleda entitetot(student) i CRUD operaciite za istiot)->
+// -> databaza (Kaj nas toa ke bide fajlot students.json)
+
+const express = require("express");
+
+const {
+    getForm,
+    postForm,
+    getStudents,
+    getDelete,
+    getEdit,
+    postEdit
+} = require("./controllers/formular");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true })); //body-parser
+// It is used when we have req.body => POST, PUT, PATCH, DELETE
+// When extended is TRUE req.body can be any type (string, object, array, number, boolean, undefined, null)
+// When extended is FALSE req.body can be only string or array
+app.use(express.static("public")); // You will understand this more when you get to React.js
+// static assets reffer to files that are served to the client without any modification by the server
+// public folder contains data that the server will not process
+
+app.get("/form", getForm);
+app.post("/form", postForm);
+app.get("/students", getStudents);
+app.get("/delete", getDelete);
+app.get("/edit", getEdit);
+app.post("/edit", postEdit);
+
+app.listen(8000, (err) => {
+    if (err) console.log(err);
+    console.log("Server started at port: 8000");
+});
+
